f6fbcce6462fe904316fe6fe5c3c467fbf81328b
Kuy Mainwaring <kuy@outbreak.co>
2014-03-09 16:02:22 -0700

    Initial commit. Build params set for Cortex-M3.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Zlib for ARM Cortex M
+
+This is a concise Zlib library (.a GNU gcc archive) for ARM Cortex-M (M0, M0+, M1, M3, M4)
+
+The "kitchen-sink" build is about 43K of code.  Real-world use and optimization will be less.
+Link to your final firmware with gcc option `-Wl,--gc-sections`.
+
+This adheres to Zlib's license.  Additions unique to this repository are released to the public domain.
+See README file for Zlib's license.
+
+See [Zlib homepage](http://www.zlib.net/) for more info.
---
+++ build.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+if [ -f "toolpath" ]; then
+	toolPath=$(cat toolpath)
+	PATH="$toolPath:$PATH"
+fi
+
+mcu="cortex-m3"	# "cortex-m0"
+arch="armv7-m"	# "armv6-m" for Cortex-M0/+
+
+sources=`ls *.c`
+
+mkdir -p obj
+rm obj/*
+
+objs=""
+count=0
+for s in $sources; do
+	f=$(basename $s | sed -E 's/(^.*)\.[a-zA-Z]+$/\1/')
+	o="obj/$count-$f.$arch.o"
+	count=$(($count + 1))
+
+	echo "Building $s => $o"
+	arm-none-eabi-gcc -Wall -Wno-switch -nostdlib -nodefaultlibs -fno-exceptions \
+		-g -Os -mthumb -march=$arch -mcpu=$mcu -Wno-attributes \
+		--function-sections \
+		-I . \
+		-o $o -x c -c $s
+
+		if [ $? -ne 0 ]; then
+			echo "Compiling failed."
+			exit -1
+		fi
+	objs="$objs $o"
+done
+
+echo Linking...
+# use single-object pre-link
+arm-none-eabi-ld -o obj/zlib.$arch.o -r $objs
+
+if [ $? -ne 0 ]; then
+	echo "Linking failed."
+	exit -1
+fi
+
+echo Archiving...
+arm-none-eabi-ar r obj/zlib.$arch.a obj/zlib.$arch.o
+
+if [ $? -ne 0 ]; then
+	echo "Archiving failed."
+	exit -1
+fi
+
+# report symbols
+arm-none-eabi-size obj/zlib.$arch.a > obj/zlib.$arch.report.txt
+arm-none-eabi-nm obj/zlib.$arch.a >> obj/zlib.$arch.report.txt
+arm-none-eabi-objdump -d obj/zlib.armv7-m.o >> obj/zlib.$arch.report.txt
+
+cat obj/zlib.$arch.size.txt
+
+# link to final firmware with -Wl,--gc-sections
+
+exit 0
